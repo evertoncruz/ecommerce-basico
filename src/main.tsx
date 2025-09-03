d360c3dd2749fe52cddfb7eb028caa2228e25dba
Everton Cruz <cruz.everton@gmail.com>
2025-09-03 11:51:42 -0300

    return graphql to listem localhost

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import './styles/app.css';
 import { HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: '/api/graphql' }),
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
   cache: new InMemoryCache(), // O cache gerencia os dados das suas queries
 });
 
